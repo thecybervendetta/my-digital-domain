bb79354141fae2d7f9f73efd969b927da643f438
Richard Uwumwonse <richieeacey@gmail.com>
2026-04-11 13:20:53 +0100

    Add comment to initiate repository in config
    Added a comment to initiate the repository.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss";
 
+//initiate repo
+
 export default {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
